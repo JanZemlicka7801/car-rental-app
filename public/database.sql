fdfc8d1767e5ad6351da87e53a3f0ce3309fb559
JanZemlicka7801 <jan.zemlicka1@gmail.com>
2024-05-01 21:19:57 +0100

    Created a databse file for storing tables and commands that will be putted into a database mysql.

---
+++ public/database.sql
@@ -0,0 +1,18 @@
+CREATE TABLE cars (
+    car_id INT PRIMARY KEY AUTO_INCREMENT,
+    brand VARCHAR(50) NOT NULL,
+    model VARCHAR(50) NOT NULL,
+    year INT NOT NULL,
+    color VARCHAR(20) NOT NULL,
+    mileage INT NOT NULL,
+    daily_rental_rate DECIMAL(10, 2) NOT NULL,
+    image_url VARCHAR(255) NOT NULL,
+    available BOOLEAN NOT NULL DEFAULT TRUE
+);
+
+CREATE TABLE users (
+    id INT PRIMARY KEY AUTO_INCREMENT,
+    username VARCHAR(50) NOT NULL,
+    email VARCHAR(100) UNIQUE NOT NULL,
+    password VARCHAR(255) NOT NULL
+);
